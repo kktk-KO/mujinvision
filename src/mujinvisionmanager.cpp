bfc6b73c24334b1c79410664cdc13f8191c8641f
Huan Liu <liuhuanjim013@gmail.com>
2016-08-29 10:39:34 +0900

    clear old images when exception happens during starting capturing

--- src/mujinvisionmanager.cpp
+++ src/mujinvisionmanager.cpp
@@ -325,6 +325,11 @@ void MujinVisionManager::_StartAndGetCaptureHandle(const std::vector<std::string
         } catch(const std::exception& ex) {
             MUJIN_LOG_ERROR("caught exception " << ex.what());
             MUJIN_LOG_ERROR("failed to start capturing for cameras " << __GetString(tostart));
+            MUJIN_LOG_WARN("need to clear out old images");
+            _pImagesubscriberManager->Reset();
+            _lastresultimages.clear();
+            _lastcolorimages.clear();
+            _lastdepthimages.clear();
         }
     } else {
         MUJIN_LOG_INFO("capturing of cameras " << __GetString(cameranames) << " have already been started");

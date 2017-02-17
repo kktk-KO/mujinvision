b5976517219855b331c1a03697be4b68e98bddc9
Huan Liu <liuhuanjim013@gmail.com>
2017-02-17 17:40:28 +0900

    fixed typo

--- src/mujinvisionmanager.cpp
+++ src/mujinvisionmanager.cpp
@@ -2413,7 +2413,7 @@ void MujinVisionManager::_SendExecutionVerificationPointCloudThread(SendExecutio
                 hasRobotExecutionStarted = _bHasRobotExecutionStarted;
             }
             boost::this_thread::sleep(boost::posix_time::milliseconds(20));
-        } while (!hasRobotExecutionStarted && !_bStopControllerMonitorThread);
+        } while (!hasRobotExecutionStarted && !_bStopExecutionVerificationPointCloudThread);
 
         FalseSetter turnoffstatusvar(_bIsExecutionVerificationPointCloudRunning);
         std::vector<std::string> cameranames = params.cameranames;
